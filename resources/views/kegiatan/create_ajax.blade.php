a88d7ecb70d59ba2db23f1d7f277f805aa547d37
Oddis Nur Alifathur Razaaq <oddisnur@gmail.com>
2024-11-11 10:35:42 +0700

    Oddis

--- resources/views/kegiatan/create_ajax.blade.php
+++ resources/views/kegiatan/create_ajax.blade.php
@@ -32,7 +32,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                <button type="button" class="btn btn-warning" data-dismiss="modal">Batal</button>
                 <button type="submit" class="btn btn-primary">Simpan</button>
             </div>
         </div>
