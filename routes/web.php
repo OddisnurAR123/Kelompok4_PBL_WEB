07dd8bdbb27a54d6134b2bb08f9d64b32f179d84
silmymaulia <silmy.smd@gmail.com>
2024-11-11 09:46:49 +0700

    silmy

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,9 @@ use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JenisPenggunaController;
 use App\Models\JenisPenggunaModel;
-<<<<<<< HEAD
 use App\Http\Controllers\JenisKegiatanController;
 use App\Http\Controllers\KegiatanController;
-=======
 use App\Http\Controllers\KategoriKegiatanController;
->>>>>>> 9b31000cca6acc8de47d36fdbc1d2765a6e8b106
 
 /*
 |--------------------------------------------------------------------------
@@ -24,7 +21,7 @@ use App\Http\Controllers\KategoriKegiatanController;
 
 Route::get('/', [WelcomeController::class, 'index']);
 
-<<<<<<< HEAD
+
 // Route::middleware(['authorize:ADM,MNG'])->group(function () {
     Route::get('/jenis_pengguna', [JenisPenggunaController::class, 'index'])->name('jenis_pengguna.index');  
     // Route::get('/level/create_ajax', [LevelController::class, 'create_ajax']);
@@ -58,26 +55,5 @@ Route::get('/', [WelcomeController::class, 'index']);
     Route::post('/jenis_kegiatan/import_ajax', [JenisKegiatanController::class, 'import_ajax']);
     Route::get('/jenis_kegiatan/export_excel', [JenisKegiatanController::class, 'export_excel']);
     Route::get('/jenis_kegiatan/export_pdf', [JenisKegiatanController::class, 'export_pdf']);
-=======
-    Route::get('/kategori-kegiatan', [KategoriKegiatanController::class, 'index']);
-    Route::post('/kategori-kegiatan/list', [KategoriKegiatanController::class, 'list']);
-    Route::get('/kategori-kegiatan/create', [KategoriKegiatanController::class, 'create']);
-    Route::post('/kategori-kegiatan', [KategoriKegiatanController::class, 'store']);
-    Route::get('/kategori-kegiatan/create_ajax', [KategoriKegiatanController::class, 'create_ajax']);
-    Route::post('/kategori-kegiatan/store_ajax', [KategoriKegiatanController::class, 'store_ajax']);
-    Route::get('/kategori-kegiatan/{id}', [KategoriKegiatanController::class, 'show']);
-    Route::get('/kategori-kegiatan/{id}/show_ajax', [KategoriKegiatanController::class, 'show_ajax']);
-    Route::get('/kategori-kegiatan/{id}/edit', [KategoriKegiatanController::class, 'edit']);
-    Route::put('/kategori-kegiatan/{id}', [KategoriKegiatanController::class, 'update']);
-    Route::get('/kategori-kegiatan/{id}/edit_ajax', [KategoriKegiatanController::class, 'edit_ajax']);
-    Route::put('/kategori-kegiatan/{id}/update_ajax', [KategoriKegiatanController::class, 'update_ajax']);
-    Route::get('/kategori-kegiatan/{id}/delete_ajax', [KategoriKegiatanController::class, 'confirm_ajax']);
-    Route::delete('/kategori-kegiatan/{id}/delete_ajax', [KategoriKegiatanController::class, 'delete_ajax']);
-    Route::delete('/kategori-kegiatan/{id}', [KategoriKegiatanController::class, 'destroy']);
-    Route::get('/kategori-kegiatan/import', [KategoriKegiatanController::class, 'import']);
-    Route::post('/kategori-kegiatan/import_ajax', [KategoriKegiatanController::class, 'import_ajax']);
-    Route::get('/kategori-kegiatan/export_excel', [KategoriKegiatanController::class, 'export_excel']);
-    Route::get('/kategori-kegiatan/export_pdf', [KategoriKegiatanController::class, 'export_pdf']);
->>>>>>> 9b31000cca6acc8de47d36fdbc1d2765a6e8b106
 
 
