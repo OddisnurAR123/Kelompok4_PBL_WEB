4976359ac4e16d13ece263b86bc79d6d17bef7ec
TriandikaNursyamsaDarmawan <andikadarmawan43@gmail.com>
2024-11-18 11:08:39 +0700

    Triandika

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="card card-outline card-primary">
     <div class="card-header">
-        <h3 class="card-title">Pengaturan Profil</h3>
+        <h3 class="card-title">Pengaturan Profile</h3>
     </div>
     <div class="card-body">
         @if(session('success'))
