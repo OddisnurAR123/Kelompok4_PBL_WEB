f4f4a255339d01eb5c50763b3045ca8c27619194
zefanya <zefanyatiomora@gmail.com>
2024-11-23 19:28:21 +0700

    zefanya

--- app/Http/Controllers/AgendaKegiatanController.php
+++ app/Http/Controllers/AgendaKegiatanController.php
@@ -90,16 +90,14 @@ class AgendaKegiatanController extends Controller
         $agenda->delete(); // Hapus agenda
         return redirect()->route('agenda.index')->with('success', 'Agenda berhasil dihapus!');
     }
-    public function create_ajax()
-    {
-        // Ambil data yang diperlukan untuk form
-        $kegiatans = KegiatanModel::select('id_kegiatan', 'nama_kegiatan')->get(); 
-        $jenisPenggunas = JenisPenggunaModel::select('id_jenis_pengguna', 'nama_jenis_pengguna')->get();
-        $jabatanKegiatans = JabatanKegiatanModel::select('id_jabatan_kegiatan', 'nama_jabatan_kegiatan')->get();
-    
-        // Mengirimkan response dalam bentuk HTML untuk modal form
-        return view('agenda.create_ajax', compact('kegiatan', 'jenisPengguna', 'jabatanKegiatan'));
-    }
-    
+    public function create()
+{
+    $kegiatans = KegiatanModel::all();
+    $jenisPenggunas = JenisPenggunaModel::all();
+    $jabatanKegiatans = JabatanKegiatanModel::all();
+
+    return view('agenda.create', compact('kegiatans', 'jenisPenggunas', 'jabatanKegiatans'));
+}
+
 
 }
