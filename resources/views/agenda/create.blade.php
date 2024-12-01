6aa44d9fc899fc8940e048820383db1c0dae457c
zefanya <zefanyatiomora@gmail.com>
2024-12-02 00:03:41 +0700

    zefanya

--- resources/views/agenda/create.blade.php
+++ resources/views/agenda/create.blade.php
@@ -1,9 +1,9 @@
-<form action="{{ url('/agenda') }}" method="POST" id="form-tambah-agenda">
+<form action="{{ route('agenda.store') }}" method="POST" id="form-create-agenda" enctype="multipart/form-data">
     @csrf
     <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Agenda Kegiatan</h5>
+                <h5 class="modal-title">Tambah Data Agenda</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -12,12 +12,12 @@
                 <div class="form-group">
                     <label>Kode Agenda</label>
                     <input type="text" name="kode_agenda" id="kode_agenda" class="form-control" required>
-                    <small id="error-kode_agenda" class="error-text form-text text-danger"></small>
+                    <small id="error-kode_agenda" class="text-danger"></small>
                 </div>
                 <div class="form-group">
                     <label>Nama Agenda</label>
                     <input type="text" name="nama_agenda" id="nama_agenda" class="form-control" required>
-                    <small id="error-nama_agenda" class="error-text form-text text-danger"></small>
+                    <small id="error-nama_agenda" class="text-danger"></small>
                 </div>
                 <div class="form-group">
                     <label>Kegiatan</label>
@@ -27,12 +27,12 @@
                             <option value="{{ $item->id_kegiatan }}">{{ $item->nama_kegiatan }}</option>
                         @endforeach
                     </select>
-                    <small id="error-id_kegiatan" class="error-text form-text text-danger"></small>
+                    <small id="error-id_kegiatan" class="text-danger"></small>
                 </div>
                 <div class="form-group">
                     <label>Tempat Agenda</label>
                     <input type="text" name="tempat_agenda" id="tempat_agenda" class="form-control" required>
-                    <small id="error-tempat_agenda" class="error-text form-text text-danger"></small>
+                    <small id="error-tempat_agenda" class="text-danger"></small>
                 </div>
                 <div class="form-group">
                     <label>Jenis Pengguna</label>
@@ -42,32 +42,32 @@
                             <option value="{{ $item->id_jenis_pengguna }}">{{ $item->nama_jenis_pengguna }}</option>
                         @endforeach
                     </select>
-                    <small id="error-id_jenis_pengguna" class="error-text form-text text-danger"></small>
+                    <small id="error-id_jenis_pengguna" class="text-danger"></small>
                 </div>
                 <div class="form-group">
                     <label>Jabatan Kegiatan</label>
-                    <select name="id_jabatan_kegiatan" id="id_jabatan_kegiatan" class="form-control" required>
+                    <select name="id_jabatan_kegiatan" id="id_jabatan_kegiatan" class="form-control">
                         <option value="">Pilih Jabatan Kegiatan</option>
                         @foreach($jabatanKegiatan as $item)
                             <option value="{{ $item->id_jabatan_kegiatan }}">{{ $item->nama_jabatan_kegiatan }}</option>
                         @endforeach
                     </select>
-                    <small id="error-id_jabatan_kegiatan" class="error-text form-text text-danger"></small>
+                    <small id="error-id_jabatan_kegiatan" class="text-danger"></small>
                 </div>
                 <div class="form-group">
                     <label>Bobot Anggota</label>
                     <input type="number" name="bobot_anggota" id="bobot_anggota" class="form-control" required>
-                    <small id="error-bobot_anggota" class="error-text form-text text-danger"></small>
+                    <small id="error-bobot_anggota" class="text-danger"></small>
                 </div>
                 <div class="form-group">
                     <label>Tanggal Agenda</label>
                     <input type="date" name="tanggal_agenda" id="tanggal_agenda" class="form-control" required>
-                    <small id="error-tanggal_agenda" class="error-text form-text text-danger"></small>
+                    <small id="error-tanggal_agenda" class="text-danger"></small>
                 </div>
                 <div class="form-group">
                     <label>Deskripsi</label>
                     <textarea name="deskripsi" id="deskripsi" class="form-control"></textarea>
-                    <small id="error-deskripsi" class="error-text form-text text-danger"></small>
+                    <small id="error-deskripsi" class="text-danger"></small>
                 </div>
             </div>
             <div class="modal-footer">
@@ -79,57 +79,33 @@
 </form>
 
 <script>
-   $(document).ready(function() {
-    $("#form-tambah-agenda").validate({
-        rules: {
-            kode_agenda: { required: true, minlength: 3 },
-            nama_agenda: { required: true, minlength: 3 },
-            id_kegiatan: { required: true },
-            tempat_agenda: { required: true, minlength: 3 },
-            id_jenis_pengguna: { required: true },
-            id_jabatan_kegiatan: { required: true }, // Ubah menjadi tidak required
-            bobot_anggota: { required: true, number: true },
-            tanggal_agenda: { required: true, date: true }
-        },
-        submitHandler: function(form) {
-            $.ajax({
-                url: form.action,
-                type: form.method,
-                data: $(form).serialize(),
-                success: function(response) {
-                    if (response.status) {
-                        $('#myModal').modal('hide');
-                        Swal.fire({
-                            icon: 'success',
-                            title: 'Berhasil',
-                            text: response.message
-                        });
-                        dataAgenda.ajax.reload();
-                    } else {
-                        $('.error-text').text('');
-                        $.each(response.msgField, function(prefix, val) {
-                            $('#error-' + prefix).text(val[0]);
-                        });
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Terjadi Kesalahan',
-                            text: response.message
-                        });
+    $(document).ready(function() {
+        $("#form-create-agenda").validate({
+            submitHandler: function(form) {
+                $.ajax({
+                    url: form.action,
+                    type: form.method,
+                    data: new FormData(form),
+                    contentType: false,
+                    processData: false,
+                    success: function(response) {
+                        if (response.status) {
+                            Swal.fire('Berhasil', response.message, 'success');
+                            $('#modal-master').modal('hide');
+                            // Reload data jika menggunakan DataTables
+                            dataAgenda.ajax.reload();
+                        } else {
+                            $.each(response.msgField, function(key, value) {
+                                $('#error-' + key).text(value[0]);
+                            });
+                        }
+                    },
+                    error: function(xhr) {
+                        Swal.fire('Error', 'Terjadi kesalahan saat menyimpan data.', 'error');
                     }
-                }
-            });
-            return false;
-        },
-        errorElement: 'span',
-        errorPlacement: function(error, element) {
-            error.addClass('invalid-feedback');
-            element.closest('.form-group').append(error);
-        },
-        highlight: function(element, errorClass, validClass) {
-            $(element).addClass('is-invalid');
-        },
-        unhighlight: function(element, errorClass, validClass) {
-            $(element).removeClass('is-invalid');
-        }
+                });
+                return false;
+            }
+        });
     });
-});
+</script>
