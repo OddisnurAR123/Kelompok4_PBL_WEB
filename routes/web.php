0c7475d11d2e2a605bbf3080f58fc13f28c8e785
Oddis Nur Alifathur Razaaq <oddisnur@gmail.com>
2024-12-12 12:10:00 +0700

    Oddis

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
     // Detail Kegiatan Routes
     Route::get('/detail_kegiatan', [DetailKegiatanController::class, 'index'])->name('detail_kegiatan.index');
     Route::post('/detail_kegiatan/list', [DetailKegiatanController::class, 'list']);
-    Route::get('/detail_agenda/create/{id_kegiatan}/{id_agenda}', [DetailAgendaController::class, 'create'])->name('detail_agenda.create');
+    Route::get('/detail_kegiatan/create', [DetailKegiatanController::class, 'create'])->name('detail_kegiatan.create');
     Route::post('/detail_kegiatan/store', [DetailKegiatanController::class, 'store'])->name('detail_kegiatan.store');
     Route::get('/detail_kegiatan/{id}/show', [DetailKegiatanController::class, 'show'])->name('detail_kegiatan.show');
     Route::get('/detail_kegiatan/{id}/edit', [DetailKegiatanController::class, 'edit'])->name('detail_kegiatan.edit');
