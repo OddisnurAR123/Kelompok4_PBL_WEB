ebc20a769ff35e8209c45c14e2ede6bd188e327f
Oddis Nur Alifathur Razaaq <oddisnur@gmail.com>
2024-12-15 14:03:19 +0700

    Oddis

--- routes/web.php
+++ routes/web.php
@@ -76,16 +76,8 @@ Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
     Route::delete('/kegiatan/{id}/delete', [KegiatanController::class, 'delete'])->name('kegiatan.delete');
     Route::get('/kegiatan/export_pdf', [KegiatanController::class, 'export_pdf'])->name('kegiatan.export.pdf');
     Route::get('kegiatan/{id_kegiatan}/unduh_surat', [KegiatanController::class, 'unduhSuratTugas'])->name('kegiatan.downloadDraft');
-    // Route untuk menampilkan form upload
     Route::get('/kegiatan/{id}/upload_surat_tugas', [KegiatanController::class, 'showUploadForm'])->name('kegiatan.uploadForm');
-    // Route untuk menangani upload file surat tugas
     Route::post('/kegiatan/{id}/upload_surat_tugas', [KegiatanController::class, 'upload'])->name('kegiatan.upload');
-    // Route untuk mengunduh file surat tugas
-    //Route::get('kegiatan/data', [KegiatanController::class, 'getData'])->name('kegiatan.data');
-    // Route untuk mengunduh surat tugas
-    //Route::get('kegiatan/{id}/unduh', [KegiatanController::class, 'suratTugas'])->name('kegiatan.unduh');
-    // Route untuk menampilkan surat tugas
-    Route::get('kegiatan/{id}/surat-tugas', [KegiatanController::class, 'suratTugas'])->name('kegiatan.surat_tugas');
     // web.php
     Route::get('kegiatan_pimpinan', [KegiatanController::class, 'index'])->name('index');
     Route::post('/kegiatan_pimpinan/list', [KegiatanController::class, 'list']);
